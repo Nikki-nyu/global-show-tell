e36cad96d50d07d6bbea2a95fef8014b9330b0f9
Ke Ning <kn2132@nyu.edu>
2025-01-19 20:53:22 +0800

    add: opencall

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/global-show-tell/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html', // Main app entry
+        opencall: './public/opencall/index.html', // Additional HTML page
+      },
+    },
+  },
 })
